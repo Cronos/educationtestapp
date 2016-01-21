a236a806e2c468794527eadb49f1d026565ab2c5
Vitaliy Voropaev <vvv@anahoret.com>
2016-01-21 16:10:54 +0200

    NSJSONSerialization temp commit

---
+++ EducationTestApp/Categories/NSJSONSerialization+Swizzling.h
@@ -0,0 +1,13 @@
+//
+//  NSJSONSerialization+Swizzling.h
+//  EducationTestApp
+//
+//  Created by Voropaev Vitali on 20.01.16.
+//  Copyright © 2016 Voropaev Vitali. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSJSONSerialization (Swizzling)
+
+@end
