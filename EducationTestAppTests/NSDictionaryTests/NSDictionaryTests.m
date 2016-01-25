a16e56e404220f9734ce79938959e973167b09ac
Vitaliy Voropaev <vvv@anahoret.com>
2016-01-25 18:28:39 +0200

    remove unused tests

--- EducationTestAppTests/NSDictionaryTests/NSDictionaryTests.m
+++ EducationTestAppTests/NSDictionaryTests/NSDictionaryTests.m
@@ -7,8 +7,6 @@
 //
 
 #import <XCTest/XCTest.h>
-#import "NSDictionary+NSNull.h"
-#import "EDANull.h"
 
 @interface NSDictionaryTests : XCTestCase
 
@@ -18,58 +16,10 @@
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
-}
-
-- (void)testExchangeValueForKeyMethod {
-    
-    NSArray *keys = @[@"keyNull", @"keyObject", @"keyArray", @"keyString"];
-    NSDictionary *dictionary = @{ keys[0] : [NSNull null],
-                                  keys[1] : [NSObject new],
-                                  keys[2] : [NSArray new],
-                                  keys[3] : [NSString new]
-                                  };
-    
-    id value = [dictionary valueForKey:keys[0]];
-    XCTAssertTrue([value isMemberOfClass:[EDANull class]], @"value must be EDANull class");
-    XCTAssertTrue([value isKindOfClass:[NSNull class]], @"value must be kind of NSNull class");
-    
-    value = [dictionary valueForKey:keys[1]];
-    XCTAssertTrue([value isKindOfClass:[NSObject class]], @"value must be NSObject class");
-    
-    value = [dictionary valueForKey:keys[2]];
-    XCTAssertTrue([value isKindOfClass:[NSArray class]], @"value must be NSArray class");
-    
-    value = [dictionary valueForKey:keys[3]];
-    XCTAssertTrue([value isKindOfClass:[NSString class]], @"value must be NSString class");
-}
-
-- (void)testExchangeObjectForKeyedSubscriptMethod {
-    
-    NSArray *keys = @[@"keyNull", @"keyObject", @"keyArray", @"keyString"];
-    NSDictionary *dictionary = @{ keys[0] : [NSNull null],
-                                  keys[1] : [NSObject new],
-                                  keys[2] : [NSArray new],
-                                  keys[3] : [NSString new]
-                                  };
-    
-    id value = dictionary[keys[0]];
-    XCTAssertTrue([value isMemberOfClass:[EDANull class]], @"value must be EDANull class instead %@", [value class]);
-    XCTAssertTrue([value isKindOfClass:[NSNull class]], @"value must be kind of NSNull class");
-    
-    value = dictionary[keys[1]];
-    XCTAssertTrue([value isKindOfClass:[NSObject class]], @"value must be NSObject class");
-    
-    value = dictionary[keys[2]];
-    XCTAssertTrue([value isKindOfClass:[NSArray class]], @"value must be NSArray class");
-    
-    value = dictionary[keys[3]];
-    XCTAssertTrue([value isKindOfClass:[NSString class]], @"value must be NSString class");
 }
 
 @end
--- EducationTestAppTests/NSNull/NSNullTests.m
+++ EducationTestAppTests/NSNull/NSNullTests.m
@@ -17,51 +17,7 @@
 @implementation NSNullTests
 
 #pragma mark -
-#pragma methods of NSNull
-
-- (void)testNullInstanceMethodList {
-    
-    Class objectClass = [NSNull superclass];
-    unsigned int methodCount = 0;
-    Method *methods = class_copyMethodList(objectClass, &methodCount);
-    
-    NSLog(@"Found %d methods on %s\n", methodCount, class_getName(objectClass));
-    
-    for (unsigned int i = 0; i < methodCount; i++) {
-        Method method = methods[i];
-        
-        NSLog(@"\t'%s' has instance-method named '%s' of encoding '%s'\n",
-              class_getName(objectClass),
-              sel_getName(method_getName(method)),
-              method_getTypeEncoding(method));
-    }
-    
-    free(methods);
-}
-
-- (void)testNullClassMethodList {
-    
-    Class objectClass = [NSNull class];
-    unsigned int methodCount = 0;
-    
-    Method *methods = class_copyMethodList(object_getClass(objectClass), &methodCount);
-    
-    NSLog(@"Found %d methods on %s\n", methodCount, class_getName(objectClass));
-    
-    for (unsigned int i = 0; i < methodCount; i++) {
-        Method method = methods[i];
-        
-        NSLog(@"\t'%s' has class-method named '%s' of encoding '%s'\n",
-              class_getName(objectClass),
-              sel_getName(method_getName(method)),
-              method_getTypeEncoding(method));
-    }
-    
-    free(methods);
-}
-
-#pragma mark -
-#pragma Swizzling
+#pragma mark
 
 - (void)testReturnEDANull {
     id null = [NSNull null];
