b517020bf01714d2e8230e82967a3ab7b045fc8d
Vitaliy Voropaev <vvv@anahoret.com>
2016-02-03 14:40:42 +0200

    separate tests for register/unregister custom classes

--- EducationTestAppTests/NSObject/NSObject+EDASubclassesTests.m
+++ EducationTestAppTests/NSObject/NSObject+EDASubclassesTests.m
@@ -1,5 +1,5 @@
 //
-//  NSObject+SubclassesTests.m
+//  NSObject+EDASubclassesTests.m
 //  EducationTestApp
 //
 //  Created by Voropaev Vitali on 19.01.16.
@@ -9,6 +9,7 @@
 #import <XCTest/XCTest.h>
 #import "NSObject+EDARuntime.h"
 #import "NSArray+EDAExtentions.h"
+#import "EDAMock.h"
 
 @interface NSObject_EDASubclassesTests : XCTestCase
 
@@ -17,11 +18,9 @@
 @implementation NSObject_EDASubclassesTests
 
 - (void)testMetaclass {
-    NSArray *customClassNames = [self customClassNames:@"EDACustomClass" withCapacity:3];
-    Class parentClass = [NSObject class];
-    for (NSString *name in customClassNames) {
-        parentClass = [parentClass registerClassWithName:name];
-    }
+    NSArray *customClassNames = [EDAMock customClassNames:@"EDACustomClass" withCapacity:3];
+
+    [EDAMock registerCustomClassesWithNames:customClassNames withRootClass:[NSObject class]];
     
     NSArray *reverseNames = [customClassNames reverse];
     [reverseNames enumerateObjectsUsingBlock:^(NSString  * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
@@ -31,18 +30,16 @@
         metaclass = [metaclass metaclass];
         XCTAssertEqualObjects(NSStringFromClass(metaclass), NSStringFromClass([NSObject class]), @"Metaclass for metaclass must be %@ class", NSStringFromClass([NSObject class]));
     }];
+    
     for (NSString *name in reverseNames) {
         [NSObject unregisterClassWithName:name];
     }
 }
 
 - (void)testSubclasses {
-    NSArray *customClassNames = [self customClassNames:@"EDACustomClass" withCapacity:4];
+    NSArray *customClassNames = [EDAMock customClassNames:@"EDACustomClass" withCapacity:4];
 
-    Class parentClass = [NSObject class];
-    for (NSString *name in customClassNames) {
-        parentClass = [parentClass registerClassWithName:name];
-    }
+    [EDAMock registerCustomClassesWithNames:customClassNames withRootClass:[NSObject class]];
     
     NSArray *reverseNames = [customClassNames reverse];
     [reverseNames enumerateObjectsUsingBlock:^(NSString  *  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
@@ -53,44 +50,6 @@
     for (NSString *name in reverseNames) {
         [NSObject unregisterClassWithName:name];
     }
-}
-
-- (void)testCreateEDACustomClass {
-    NSString *customClassName = @"EDATestClass";
-    Class class = [NSObject registerClassWithName:customClassName];
-    XCTAssertNotNil(class, @"Class %@ not registered", customClassName);
-    
-    id object = [class new];
-    
-    XCTAssertNotNil(object, @"Class %@ not initialized", customClassName);
-    XCTAssertTrue([object isMemberOfClass:class], @"Object must be member of %@ class", customClassName);
-    XCTAssertTrue([object isKindOfClass:[NSObject class]], @"Object must be kind of %@ class", [NSObject class]);
-    
-    object = nil;
-    [NSObject unregisterClassWithName:customClassName];
-}
-
-- (void)testUnregisterCustomClasses {
-    NSArray *customClassNames = [self customClassNames:@"EDACustomClass" withCapacity:4];
-    
-    Class parentClass = [NSObject class];
-    for (NSString *name in customClassNames) {
-        parentClass = [parentClass registerClassWithName:name];
-    }
-    
-    NSArray *reverseNames = [customClassNames reverse];
-    for (NSString *name in reverseNames) {
-        [NSObject unregisterClassWithName:name];
-    }
-
-}
-
-- (NSArray <NSString *> *)customClassNames:(NSString *)name withCapacity:(NSInteger)count {
-    NSMutableArray <NSString *> *array = [NSMutableArray array];
-    for (NSInteger index = 0; index < count; index++) {
-        [array addObject:[NSString stringWithFormat:@"%@%ld", name, index]];
-    }
-    return [array copy];
 }
 
 @end
