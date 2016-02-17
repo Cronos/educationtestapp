c9b2e884d0155f21d10412e85ea52bba133060f2
Voropaev Vitali <vvv@0114c0090.local>
2016-02-17 14:17:23 +0200

    add UIViewController+EDAAlert category
    - show alert

---
+++ Sources/Library/Categories/UIViewController/UIViewController+EDAAlert.m
@@ -0,0 +1,30 @@
+//
+//  UIViewController+EDAAlert.m
+//  EducationTestApp
+//
+//  Created by Voropaev Vitali on 16.02.16.
+//  Copyright © 2016 Voropaev Vitali. All rights reserved.
+//
+
+#import "UIViewController+EDAAlert.h"
+
+@implementation UIViewController (EDAAlert)
+
+- (void)showAlertWithTitle:(NSString *)title message:(NSString *)message handler:(void (^)(UIAlertAction *action))handler {
+    UIAlertController *alertController = [UIAlertController
+                                          alertControllerWithTitle:title
+                                          message:message
+                                          preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction* ok = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
+                                               handler:^(UIAlertAction * action){
+                                                   if(handler){
+                                                       handler(action);
+                                                   }
+                                               }];
+    
+    [alertController addAction:ok];
+    [self presentViewController:alertController animated:YES completion:nil];
+}
+
+@end
