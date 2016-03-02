9fbded328c584abd1e99f897f6c63dbac1bdc5f3
Voropaev Vitaliy <vvv@anahoret.com>
2016-03-02 10:09:08 +0200

    refactoring

--- Sources/Library/EDADispatch.h
+++ Sources/Library/EDADispatch.h
@@ -11,29 +11,35 @@
 
 typedef void(^EDADispatchBlock)();
 
-static inline void EDADispatchAsyncBlockInQueue(EDADispatchBlock block, dispatch_queue_t queue) {
+static inline
+void EDADispatchAsyncBlockInQueue(EDADispatchBlock block, dispatch_queue_t queue) {
     dispatch_async(queue, ^{
         block ? block() : nil;
     });
 }
 
-static inline void EDADispatchAsyncInMainQueue(EDADispatchBlock block) {
+static inline
+void EDADispatchAsyncInMainQueue(EDADispatchBlock block) {
     EDADispatchAsyncBlockInQueue(block, dispatch_get_main_queue());
 }
 
-static inline void EDADispatchAsyncWithDefaultPriority(EDADispatchBlock block) {
+static inline
+void EDADispatchAsyncWithDefaultPriority(EDADispatchBlock block) {
     EDADispatchAsyncBlockInQueue(block, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0));
 }
 
-static inline void EDADispatchAsyncWithHighPriority(EDADispatchBlock block) {
+static inline
+void EDADispatchAsyncWithHighPriority(EDADispatchBlock block) {
     EDADispatchAsyncBlockInQueue(block, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0));
 }
 
-static inline void EDADispatchAsyncWithLowPriority(EDADispatchBlock block) {
+static inline
+void EDADispatchAsyncWithLowPriority(EDADispatchBlock block) {
     EDADispatchAsyncBlockInQueue(block, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0));
 }
 
-static inline void EDADispatchAsyncWithBackgroundPriority(EDADispatchBlock block) {
+static inline
+void EDADispatchAsyncWithBackgroundPriority(EDADispatchBlock block) {
     EDADispatchAsyncBlockInQueue(block, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0));
 }
 
