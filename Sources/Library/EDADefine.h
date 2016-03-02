8b0f034372d47353d43e44796a1a98fb7e982f74
Voropaev Vitaliy <vvv@anahoret.com>
2016-03-02 11:28:54 +0200

    add common defines

---
+++ Sources/Library/EDADefine.h
@@ -0,0 +1,16 @@
+//
+//  EDADefine.h
+//  EducationTestApp
+//
+//  Created by Voropaev Vitali on 02.03.16.
+//  Copyright © 2016 Voropaev Vitali. All rights reserved.
+//
+
+#ifndef EDADefine_h
+#define EDADefine_h
+
+#define EDA_STATIC_INLINE static inline
+
+// 
+
+#endif /* EDADefine_h */
