970bbac9718682343414f847ab83d8de7c3885e3
Voropaev Vitaliy <vvv@anahoret.com>
2016-03-03 19:01:11 +0200

    update EDAObservableObject

--- Sources/Models/EDAObservableObject/EDAObservableObject.m
+++ Sources/Models/EDAObservableObject/EDAObservableObject.m
@@ -8,6 +8,65 @@
 
 #import "EDAObservableObject.h"
 
+@interface EDAObservableObject ()
+@property (nonatomic, weak)     id<NSObject>    target;
+@property (nonatomic, strong)   NSHashTable     *mutableObservers;
+
+@end
+
 @implementation EDAObservableObject
+
+#pragma mark -
+#pragma mark Class methods
+
++ (instancetype)objectWithTarget:(id<NSObject>)target {
+    return [[self alloc] initWithTarget:target];
+}
+
+#pragma mark -
+#pragma mark Initialization and Deallocation
+
+- (instancetype)init {
+    return [self initWithTarget:nil];
+}
+
+- (instancetype)initWithTarget:(id<NSObject>)target {
+    self = [super init];
+    self.mutableObservers = [NSHashTable weakObjectsHashTable];
+    self.target = target;
+    
+    return self;
+}
+
+#pragma mark -
+#pragma mark Accessors
+
+- (id)target {
+    return _target ? _target : self;
+}
+
+- (NSSet *)observers {
+    return [self.mutableObservers setRepresentation];
+}
+
+#pragma mark -
+#pragma mark Public
+
+
+- (void)setState:(EDAObjectState)state object:(id)object {
+    
+}
+
+- (EDAObserver *)observerWithObject:(id)observer {
+    return nil;
+}
+
+- (void)notifyObserversWithState:(NSUInteger)state {
+    
+}
+
+- (void)notifyObserversWithState:(NSUInteger)state object:(id)object {
+    
+}
 
 @end
