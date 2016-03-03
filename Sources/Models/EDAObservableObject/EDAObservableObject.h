56b8a14eaa047e35881f7b9728cdfbd6326523aa
Voropaev Vitaliy <vvv@anahoret.com>
2016-03-03 17:03:06 +0200

    add 'EDAObservableObject'

---
+++ Sources/Models/EDAObservableObject/EDAObservableObject.h
@@ -0,0 +1,13 @@
+//
+//  EDAObservableObject.h
+//  EducationTestApp
+//
+//  Created by Voropaev Vitali on 03.03.16.
+//  Copyright © 2016 Voropaev Vitali. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EDAObservableObject : NSObject
+
+@end
---
+++ Sources/Models/EDAObservableObject/EDAObservableObject.m
@@ -0,0 +1,13 @@
+//
+//  EDAObservableObject.m
+//  EducationTestApp
+//
+//  Created by Voropaev Vitali on 03.03.16.
+//  Copyright © 2016 Voropaev Vitali. All rights reserved.
+//
+
+#import "EDAObservableObject.h"
+
+@implementation EDAObservableObject
+
+@end
