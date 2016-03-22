9b1e71e6d7a7bb0c4773690e175c4c93e538978b
Voropaev Vitaliy <vvv@anahoret.com>
2016-03-22 11:06:42 +0200

    update tests

--- EducationTestAppTests/EDAObservingTests/EDAObserverSpec.m
+++ EducationTestAppTests/EDAObservingTests/EDAObserverSpec.m
@@ -14,6 +14,7 @@
 SPEC_BEGIN(EDAObserverSpec)
 
 describe(@"EDAObserver", ^{
+    const NSUInteger state = 99;
     __block EDAObserver *observer = nil;
     __block EDAObservableObject *observableObject = nil;
     
@@ -26,7 +27,7 @@ describe(@"EDAObserver", ^{
         observableObject = nil;
         observer = nil;
     });
-    
+
     context(@"when created with observing object", ^{
         it(@"shouldn't be nil", ^{
             [[observer shouldNot] beNil];
@@ -37,8 +38,98 @@ describe(@"EDAObserver", ^{
         });
         
         it(@"should contained in observers of observable object", ^{
-            NSLog(@"Observers: \n%@", observableObject.observers);
-            [[observableObject.observers should] containsObject:observer];
+            [[observableObject.observers should] contain:observer];
+        });
+        
+        it(@"should be valid", ^{
+            [[theValue(observer.isValid) should] beYes];
+        });
+    });
+    
+    context(@"after deallocated", ^{
+        beforeEach(^{
+            observer = nil;
+        });
+        
+        it(@"shoulde be removed from observers of observable object", ^{
+            [[observableObject.observers should] haveCountOf:0];
+        });
+    });
+    
+    context(@"when setting block for state", ^{
+        id block = ^(id observableObject, id info){
+            [NSObject description];
+        };
+        
+        beforeEach(^{
+            [observer setBlock:block forState:state];
+        });
+        
+        it(@"should return that block for that state", ^{
+            [[[observer blockForState:state] should] equal:block];
+        });
+        
+        context(@"when setting block == nil for state", ^{
+            beforeEach(^{
+                [observer setBlock:nil forState:state];
+            });
+            
+            it(@"should return nil for that state", ^{
+                [[[observer blockForState:state] should] beNil];
+            });
+        });
+    });
+    
+    context(@"when setting block for state using indexed subscript", ^{
+        id block = ^(id observableObject, id info){
+            [NSObject description];
+        };
+        
+        beforeEach(^{
+            observer[state] = block;
+        });
+        
+        it(@"should return that block for that state", ^{
+            [[observer[state] should] equal:block];
+        });
+        
+        context(@"when setting block == nil for state", ^{
+            beforeEach(^{
+                observer[state] = nil;
+            });
+            
+            it(@"should return nil for that state", ^{
+                [[observer[state] should] beNil];
+            });
+        });
+    });
+
+    context(@"when executing block for state", ^{
+        __block id sender = nil;
+        __block id receivedNotification = nil;
+        
+        id object = [NSObject new];
+        
+        beforeEach(^{
+            id block = ^(id observableObject, id info) {
+                sender = observableObject;
+                receivedNotification = info;
+            };
+            [observer setBlock:block forState:state];
+            [observer performBlockForState:state object:object];
+        });
+        
+        afterEach(^{
+            sender = nil;
+            receivedNotification = nil;
+        });
+        
+        it(@"should notify observer by sending observable object", ^{
+            [[sender should] equal:observableObject];
+        });
+        
+        it(@"should notify observer by sending object as parameter", ^{
+            [[receivedNotification should] equal:object];
         });
     });
 });
